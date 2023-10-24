6961adfbb5159f7912bccf893e33f83947b6e454
Elias Ankerhold <elias.ankerhold@gmail.com>
2023-10-24 21:04:54 +0300

    deactivated latest settings retrieval from ThingSpeak, changed API cycle

--- src/connection.h
+++ src/connection.h
@@ -5,11 +5,12 @@
 #include <ThingSpeak.h>
 
 #include <credentials.h>
+#include <fan_control.h>
 
 WiFiClient client;
 
 unsigned long api_timestamp;
-unsigned int api_cycle = 15 * 1000;
+unsigned int api_cycle = 30 * 1000;
 
 void setupWiFi()
 {
@@ -27,20 +28,23 @@ void setupWiFi()
     api_timestamp = millis();
 }
 
-bool checkWiFi()
+void getLatestSettings()
 {
-    int count = 0;
-    while (WiFi.status() != WL_CONNECTED && count < 10)
+    int stat;
+    stat = ThingSpeak.readMultipleFields(THINGSPEAK_CHANNEL_ID, THINGSPEAK_API_KEY_READ);
+
+    Serial.println("Tried getting latest PWM data, status=" + String(stat));
+
+    if (stat == 200)
     {
-        WiFi.reconnect();
-        count++;
-    }
-    if (WiFi.status() == WL_CONNECTED)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
+        Serial.println(ThingSpeak.getFieldAsString(API_TEMP_FIELD));
+        Serial.println(ThingSpeak.getFieldAsString(API_HUM_FIELD));
+        Serial.println(ThingSpeak.getFieldAsString(API_SOIL_FIELD));
+        Serial.println(ThingSpeak.getFieldAsString(API_WATER_FIELD));
+        Serial.println(ThingSpeak.getFieldAsString(API_LIGHT_FIELD));
+        Serial.println(ThingSpeak.getFieldAsString(API_EXHAUST_FIELD));
+        Serial.println(ThingSpeak.getFieldAsString(API_CIRC_FIELD));
+        target_pwm_exhaust = ThingSpeak.getFieldAsFloat(API_EXHAUST_FIELD) * 0.01;
+        target_pwm_circ = ThingSpeak.getFieldAsFloat(API_CIRC_FIELD) * 0.01;
     }
 }
